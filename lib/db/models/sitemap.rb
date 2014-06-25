d0fb5bbfa703f7bd0fff57578300e034c12171fb
Philip Smerud <phil@netlogicsps.com>
2014-06-25 09:17:49 -0700

    add validation to sitemap model

--- lib/db/models/sitemap.rb
+++ lib/db/models/sitemap.rb
@@ -1,4 +1,10 @@
 module Crawler
   class Sitemap < ActiveRecord::Base
+    before_validation :remove_trailing_slash
+    validates_uniqueness_of :url, scope: :company_id, message: "Invalid url entry. Url already exists for the company"
+
+    def remove_trailing_slash?
+      url.gsub!(/\/$/, "") if url.end_with? "/"
+    end
   end
 end
